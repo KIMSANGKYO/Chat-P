089999a654865c6d56e6152860fd209cc5162727
KIMSANGKYO <sangyoes@naver.com>
2023-05-26 14:01:04 +0900

    :sparkles: feat : 채팅 서버 연결 설정

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+
+const ENDPOINT = "http://localhost:5000";
+let socket;
 
 const Chat = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(window.location.search);
+    socket = io(ENDPOINT);
+    setName(name);
+    setRoom(room);
+    socket.emit("join", { name, room }, (err) => {
+      if (err) {
+        alert(err);
+      }
+    });
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [ENDPOINT, window.location.search]);
+
   return <div>Chat</div>;
 };
 
